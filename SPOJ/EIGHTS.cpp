ffda70c3dc5773475555558aab5cf7c185b118f9
sgdgp <sgdgp.2014@gmail.com>
2016-12-06 12:12:41 +0530

    Added EIGHTS solution (use pattern matching)

---
+++ SPOJ/EIGHTS.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin >> n;
+        cout<<(n-1)*250 + 192<<endl;
+    }
+    return 0;
+}
